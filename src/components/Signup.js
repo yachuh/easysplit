1b72c97181b831e6bdf1cbebb91c67b97397c3df
yachuh <yachuh@gmail.com>
2022-08-30 11:03:16 +0800

    feat: setup initial environment

---
+++ src/components/Signup.js
@@ -0,0 +1,34 @@
+import { useForm } from "react-hook-form";
+
+export const Signup = () => {
+    const {
+        regsiter,
+        formState: { errors },
+        getValues,
+        handleSubmit
+    } = useForm({
+        defaultValues: {
+            name:'',
+            email:'',
+            password:''
+        }
+    });
+
+    return(
+        <div>
+            <h2 className="text-2xl mb-3">Signup</h2>
+            <form>
+                <div>Name
+                    <input type="text" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
+                </div>
+                <div>Email
+                    <input type="email" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
+                </div>
+                <div>Password
+                    <input type="password" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
+                </div>
+                <input type="submit" value="Signup" className="p-2 border border-slate-700 rounded-sm"/>
+            </form>
+        </div>
+    );
+}
