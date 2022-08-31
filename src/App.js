601f2d7a1483e649de4885d3359b1e3820415852
yachuh <yachuh@gmail.com>
2022-08-31 13:46:29 +0800

    feat: account activation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import Nav from "./components/Nav";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import AccountActivationPage from "./pages/AccountActivationPage";
 
 function App() {
   return (
     <div className="container mx-auto">
       <Nav />
       <Routes>
-        <Route path="/" element={<HomePage />}/>
+        <Route index element={<HomePage />}/>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        {/* <Route path={"/#AuthMail"}>
+          <Route path="AccountActivation" element={<AccountActivationPage />} />
+        </Route> */}
+        <Route path={"AccountActivation"} element={<AccountActivationPage />} />
       </Routes>
     </div>
   );
