5eadb5e587e9019e59326c224523789aa25a368c
DoraH <poyuhq@gmail.com>
2022-08-31 10:12:26 +0800

    feat signup/login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,142 @@
+import axios from 'axios';
 import { useForm } from "react-hook-form";
 
 export const Login = () => {
-    const {
-        regsiter,
-        formState: { errors },
-        getValues,
-        handleSubmit
-    } = useForm({
+    const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            name:'',
-            email:'',
-            password:''
+            Account: '',
+            Password: ''
         }
     });
+    const onSubmit = data => {
+        console.log(data);
 
-    return(
+        const {
+            Account,
+            Password
+        } = data;
+
+        axios
+            .post(`https://easysplit.rocket-coding.com/api/User/Login`,
+                {
+                    "Account": Account,
+                    "Password": Password.trim()
+                })
+            .then(res => {
+                console.log(res);
+                console.log("登入成功");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
+        //     var myHeaders = new Headers();
+        //     myHeaders.append("Content-Type", "application/json");
+
+        //     var raw = JSON.stringify({
+        //         "Name": "Dora",
+        //         "Account": "poyuhq@gmail.com",
+        //         "Password": "VV778899"
+        //     });
+
+        //     var requestOptions = {
+        //         method: 'POST',
+        //         headers: myHeaders,
+        //         body: raw,
+        //         redirect: 'follow'
+        //     };
+
+        //     fetch("https://easysplit.rocket-coding.com/api/user/SignUp", requestOptions)
+        //         .then(response => response.text())
+        //         .then(result => console.log(result))
+        //         .catch(error => console.log('error', error));
+    }
+
+    // const {
+    //     regsiter,
+    //     formState: { errors },
+    //     getValues,
+    //     handleSubmit
+    // } = useForm({
+    //     defaultValues: {
+    //         name:'',
+    //         email:'',
+    //         password:''
+    //     }
+    // });
+
+    return (
         <div>
-            <h2 className="text-2xl mb-3">Login</h2>
-            <form>
+            <h2 className="text-2xl mb-3">Signup</h2>
+            <form
+                className="flex flex-col w-1/3"
+                onSubmit={handleSubmit(onSubmit)}>
+                <label
+                    htmlFor="Account">
+                    Email
+                </label>
+                <input
+                    className="mb-3 border border-slate-700 rounded-sm"
+                    type="email"
+                    placeholder="email"
+                    {...register("Account",
+                        {
+                            required: {
+                                value: true,
+                                message: '請輸入資料內容!'
+                            },
+                            pattern: {
+                                value: /^\S+@\S+$/i,
+                                message: "格式有誤!"
+                            }
+                        }
+                    )}
+                />
+                <p
+                    className="text-xs mb-2 text-rose-600">
+                    {errors.Account?.message}
+                </p>
+                <label
+                    htmlFor="Password">
+                    Password
+                </label>
+                <input
+                    className="mb-3 border border-slate-700 rounded-sm"
+                    type="password"
+                    placeholder="Password"
+                    {...register("Password",
+                        {
+                            required: {
+                                value: true,
+                                message: '請輸入資料內容!'
+                            },
+                            minLength: {
+                                value: 8,
+                                message: "密碼長度至少6位字元"
+                            }
+                        }
+                    )} />
+                <p className="text-xs mb-2 text-rose-600">{errors.Password?.message}</p>
+
+                <input
+                    className="p-2 border border-slate-700 rounded-sm w-1/3"
+                    value="Login"
+                    type="submit" />
+            </form>
+
+            {/* <form>
+                <div>Name
+                    <input type="text" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
+                </div>
                 <div>Email
-                    <input type="email" className="ml-2 mb-3 border border-slate-700 rounded-sm" />
+                    <input type="email" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
                 </div>
                 <div>Password
-                    <input type="password" className="ml-2 mb-3 border border-slate-700 rounded-sm" />
+                    <input type="password" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
                 </div>
-                <input type="submit" value="Login" className="p-2 border border-slate-700 rounded-sm" />
-            </form>
+                <input type="submit" value="Signup" className="p-2 border border-slate-700 rounded-sm"/>
+            </form> */}
         </div>
     );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,23 +1,153 @@
+import axios from 'axios';
 import { useForm } from "react-hook-form";
 
 export const Signup = () => {
-    const {
-        regsiter,
-        formState: { errors },
-        getValues,
-        handleSubmit
-    } = useForm({
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            name:'',
-            email:'',
-            password:''
+            Name: '',
+            Account: '',
+            Password: ''
         }
     });
+    const onSubmit = data => {
+        console.log(data);
 
-    return(
+        const {
+            Name,
+            Account,
+            Password
+        } = data;
+
+        axios
+            .post(`https://easysplit.rocket-coding.com/api/User/SignUp`,
+                {
+                    "Name": Name.trim(),
+                    "Account": Account,
+                    "Password": Password.trim()
+                })
+            .then(res => {
+                console.log(res);
+                console.log("註冊成功");
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
+        //     var myHeaders = new Headers();
+        //     myHeaders.append("Content-Type", "application/json");
+
+        //     var raw = JSON.stringify({
+        //         "Name": "Dora",
+        //         "Account": "poyuhq@gmail.com",
+        //         "Password": "VV778899"
+        //     });
+
+        //     var requestOptions = {
+        //         method: 'POST',
+        //         headers: myHeaders,
+        //         body: raw,
+        //         redirect: 'follow'
+        //     };
+
+        //     fetch("https://easysplit.rocket-coding.com/api/user/SignUp", requestOptions)
+        //         .then(response => response.text())
+        //         .then(result => console.log(result))
+        //         .catch(error => console.log('error', error));
+    }
+
+    // const {
+    //     regsiter,
+    //     formState: { errors },
+    //     getValues,
+    //     handleSubmit
+    // } = useForm({
+    //     defaultValues: {
+    //         name:'',
+    //         email:'',
+    //         password:''
+    //     }
+    // });
+
+    return (
         <div>
             <h2 className="text-2xl mb-3">Signup</h2>
-            <form>
+            <form
+                className="flex flex-col w-1/3"
+                onSubmit={handleSubmit(onSubmit)}>
+                <label
+                    htmlFor="Name">
+                    Name
+                </label>
+                <input
+                    className="mb-3 border border-slate-700 rounded-sm"
+                    type="text"
+                    placeholder="Name"
+                    {...register("Name", {
+                        required: {
+                            value: true,
+                            message: '請輸入您的姓名!'
+                        }
+                    })} />
+                <p
+                    className="text-xs mb-2 text-rose-600">
+                    {errors.Name?.message}
+                </p>
+                <label
+                    htmlFor="Account">
+                    Email
+                </label>
+                <input
+                    className="mb-3 border border-slate-700 rounded-sm"
+                    type="email"
+                    placeholder="email"
+                    {...register("Account",
+                        {
+                            required: {
+                                value: true,
+                                message: '請輸入資料內容!'
+                            },
+                            pattern: {
+                                value: /^\S+@\S+$/i,
+                                message: "格式有誤!"
+                            }
+                        }
+                    )}
+                />
+                <p
+                    className="text-xs mb-2 text-rose-600">
+                    {errors.Account?.message}
+                </p>
+                <label
+                    htmlFor="Password">
+                    Password
+                </label>
+                <input
+                    className="mb-3 border border-slate-700 rounded-sm"
+                    type="password"
+                    placeholder="Password"
+                    {...register("Password",
+                        {
+                            required: {
+                                value: true,
+                                message: '請輸入資料內容!'
+                            },
+                            minLength: {
+                                value: 8,
+                                message: "密碼長度至少6位字元"
+                            }
+                        }
+                    )} />
+                <p className="text-xs mb-2 text-rose-600">{errors.Password?.message}</p>
+
+                <input
+                    className="p-2 border border-slate-700 rounded-sm w-1/3"
+                    value="Signup"
+                    type="submit" />
+            </form>
+
+            {/* <form>
                 <div>Name
                     <input type="text" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
                 </div>
@@ -28,7 +158,7 @@ export const Signup = () => {
                     <input type="password" className="ml-2 mb-3 border border-slate-700 rounded-sm"/>
                 </div>
                 <input type="submit" value="Signup" className="p-2 border border-slate-700 rounded-sm"/>
-            </form>
+            </form> */}
         </div>
     );
 }
